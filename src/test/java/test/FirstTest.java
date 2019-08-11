f2774316ce532a54c03ba9bf7c732ca682d5e03d
Elena Ivan <elena.ivan@xing.com>
2019-08-11 19:07:39 +0200

    mvn test works

--- src/test/java/test/FirstTest.java
+++ src/test/java/test/FirstTest.java
@@ -1,4 +1,11 @@
 package test;
 
+import org.junit.Test;
+
 public class FirstTest {
+
+    @Test
+    public void display() {
+        System.out.println("Hello world. This is Elena here");
+    }
 }
